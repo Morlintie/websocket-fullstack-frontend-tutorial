708305aa912e30b78cef032ce9b62dd288a44261
Morlintie <ahmetyasintoraman@gmail.com>
2025-06-29 14:34:05 +0300

    finishing frontend part

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,3 +1,59 @@
+import "../styles/Profile/ProfilePage.css";
+import assets from "../assets/assets";
+import { useState } from "react";
+
 export function ProfilePage() {
-  return <h1>Hello form profile page!!!</h1>;
+  const [profileForm, setProfileForm] = useState({
+    name: "Martin Jhonson",
+    bio: "Hi Everyone, I am using QucikChat!",
+  });
+  const [profileURL, setProfileURL] = useState(assets.profile_martin);
+
+  function handleProfileChange(e) {
+    setProfileForm((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  return (
+    <section className="profile">
+      <div className="profile-config">
+        <h1>Profile details</h1>
+        <input
+          onChange={(e) => {
+            setProfileURL(URL.createObjectURL(e.target.files[0]));
+            const form = new FormData();
+            form.append("image", e.target.files[0]);
+          }}
+          name="image"
+          accept="image/*"
+          id="img"
+          className="profile-img-input"
+          type="file"
+        />
+        <label className="profile-label" htmlFor="img">
+          <img src={profileURL} alt="profile-picture" />
+
+          <span>Upload image</span>
+        </label>
+        <input
+          value={profileForm.name}
+          onChange={handleProfileChange}
+          name="name"
+          type="text"
+        />
+        <textarea
+          value={profileForm.bio}
+          onChange={handleProfileChange}
+          name="bio"
+          id=""
+        ></textarea>
+        <button>Save</button>
+      </div>
+      <img src={assets.logo_big} alt="logo" />
+    </section>
+  );
 }
